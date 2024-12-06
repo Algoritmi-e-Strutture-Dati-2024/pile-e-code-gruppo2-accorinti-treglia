206637efe04c3652067534dd3b8fa460fd197a12
TREGLIA RAFFAELLA <r.treglia@studenti.uniba.it>
2024-12-06 11:08:24 +0100

    Coda.cpp

--- Coda.cpp
+++ Coda.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include "ListaPuntatori.cpp"
+template <typename T>
+class Coda {
+private:
+    ListaPuntatori<T> lista;
 
+public:
+    // Inserire un elemento alla fine della coda
+    void enqueque(T valore) {
+       lista.inslista(valore, lista.lunghezza()+1);
+    }
+
+    // Rimuovi un elemento in testa alla coda e lo restituisce
+    T dequeque() {
+        T elem=lista.leggilista(1);
+        lista.canclista(1);
+        return(elem);
+    }
+
+    // restituisce il valore in testa alla coda senza rimuoverlo
+    T front() {
+          return(lista.leggilista(1));
+    }
+
+    //restituisce true se la coda è vuota altrimenti false
+    bool isEmpty(){
+      return(lista.listavuota());
+    }
+    //restituisce il numero di elementi nella coda
+    int size(){
+      return(lista.lunghezza());
+    }
+};
